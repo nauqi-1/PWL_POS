54cbe9424d0df14b9ca0b2ef2f668106488f4af0
nauqi-1 <nauqi2208@gmail.com>
2024-09-09 10:47:24 +0700

    Upload Praktikum 2.2

--- database/migrations/2024_09_09_024150_m_user.php
+++ database/migrations/2024_09_09_024150_m_user.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -12,8 +14,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_user', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id');
+            $table->unsignedBigInteger('level_id')->index(); //Foreign key harus index ternyata :3
+            $table->string('username',20)->unique();
+            $table->string('nama', 100);
+            $table->string('password');
             $table->timestamps();
+
+            $table->
         });
     }
 
