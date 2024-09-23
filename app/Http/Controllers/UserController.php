16a52f111697baaadf00eff5e3553d7f7bd0e854
nauqi-1 <nauqi2208@gmail.com>
2024-09-23 09:19:51 +0700

    Jobsheet 4 Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,15 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
+        $user = UserModel::where('username','manager9') -> firstOrFail();
+        return view('user', ['data' => $user]);
+        
+        /*
         $user=UserModel::findOr(20, ['username', 'nama' ], function() {
             abort(404);
         });
         return view('user', ['data' => $user]);
-
+        */
 
         /*
         $data= [
