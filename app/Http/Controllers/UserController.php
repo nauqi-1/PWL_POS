bf2c9e2d0707fff7e6bec5ff54b8f81e4300da43
nauqi-1 <nauqi2208@gmail.com>
2024-09-24 00:24:14 +0700

    Jobsheet 4 Prakitkum 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,51 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
+         $user = UserModel::all();
+         return view('user', ['data' => $user]);
+    
+    }
+
+    public function tambah() {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request -> password'),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id) {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request) {
+        $user = UserModel::find($id);
+        
+        $user -> username   = $request -> username;
+        $user -> nama       = $request -> nama;
+        $user -> password   = Hash::make('$request -> password');
+        $user -> level_id   = $request -> level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id) {
+        $user = UserModel::find($id);
+        $user -> delete();
+
+        return redirect('/user');
+    }
+}
+        /*
         $user = UserModel::create(
             [
                 'username' => 'manager69',
@@ -28,12 +73,8 @@ class UserController extends Controller
         $user -> wasChanged('nama'); //false
         
 
-        dd($user -> wasChanged(['nama','username'])); //true 
-        
+        dd($user -> wasChanged(['nama','username'])); //true
 
-    }
-}
-        /*
             $user = UserModel::firstOrNew(
             [
                 'username' => 'manager55',
