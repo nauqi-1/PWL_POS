3b4f37c7f6cba0c32a80e93ebcc28f00b63679e5
nauqi-1 <nauqi2208@gmail.com>
2024-10-25 20:39:49 +0700

    Revisi UTS

--- resources/views/penjualan/create_ajax.blade.php
+++ resources/views/penjualan/create_ajax.blade.php
@@ -72,117 +72,115 @@
 </form>
 
 <script>
-    $(document).ready(function() {
-        let itemIndex = 1; 
+  $(document).ready(function() {
+    let itemIndex = 1;
 
-        function updateHarga() {
-        $('.check-harga').on('change', function() {
+    function updateHarga() {
+        $(document).on('change', '.check-harga', function() {
             let selectedHarga = $('option:selected', this).data('harga');
             $(this).closest('.item-row').find('.harga').val(selectedHarga);
         });
     }
-        updateHarga();
 
-        let validator = $("#form-tambah").validate({
-            rules: {
-                pembeli: { required: true },
-                penjualan_kode: { required: true, minlength: 3 },
-                penjualan_tanggal: { required: true },
-                user_id: { required: true, number: true },
-                "items[][jumlah]": { required: true, number: true, min: 1 },
-                "items[][harga]": { required: true, number: true, min: 1 }
+    updateHarga();
+
+    let validator = $("#form-tambah").validate({
+        rules: {
+            pembeli: { required: true },
+            penjualan_kode: { required: true, minlength: 3 },
+            penjualan_tanggal: { required: true },
+            user_id: { required: true, number: true },
+            "items[][jumlah]": { required: true, number: true, min: 1 },
+            "items[][harga]": { required: true, number: true, min: 1 }
+        },
+        messages: {
+            "items[][jumlah]": {
+                required: "Jumlah barang harus diisi",
+                number: "Jumlah harus berupa angka",
+                min: "Jumlah minimal 1"
             },
-            messages: {
-                "items[][jumlah]": {
-                    required: "Jumlah barang harus diisi",
-                    number: "Jumlah harus berupa angka",
-                    min: "Jumlah minimal 1"
-                },
-                "items[][harga]": {
-                    required: "Harga harus diisi",
-                    number: "Harga harus berupa angka",
-                    min: "Harga minimal 1"
-                }
-            },
-            submitHandler: function(form) {
-                $.ajax({
-                    url: form.action,
-                    type: form.method,
-                    data: $(form).serialize(),
-                    success: function(response) {
-                        if (response.status) {
-                            $('#myModal').modal('hide');
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Berhasil',
-                                text: response.message
-                            });
-                            dataPenjualan.ajax.reload(); 
-                        } else {
-                            $('.error-text').text('');
-                            $.each(response.msgField, function(prefix, val) {
-                                $('#error-' + prefix).text(val[0]);
-                            });
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Terjadi Kesalahan',
-                                text: response.message
-                            });
-                        }
-                    }
-                });
-                return false; 
-            },
-            errorElement: 'span',
-            errorPlacement: function(error, element) {
-                error.addClass('invalid-feedback');
-                element.closest('.form-group').append(error);
-            },
-            highlight: function(element, errorClass, validClass) {
-                $(element).addClass('is-invalid');
-            },
-            unhighlight: function(element, errorClass, validClass) {
-                $(element).removeClass('is-invalid');
+            "items[][harga]": {
+                required: "Harga harus diisi",
+                number: "Harga harus berupa angka",
+                min: "Harga minimal 1"
             }
-        });
-
-        // Adding new item row
-        $('#add-item').on('click', function() {
-            const newItemRow = `
-                <div class="row item-row">
-                    <div class="col-md-4">
-                        <label>Nama Barang</label>
-                        <select name="items[${itemIndex}][barang_id]" class="form-control" required>
-                            <option value="">- Pilih Barang -</option>
-                            @foreach($barang as $item)
-                                <option value="{{ $item->barang_id }}">{{ $item->barang_nama }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Jumlah</label>
-                        <input type="number" name="items[${itemIndex}][jumlah]" class="form-control" required>
-                    </div>
-                    <div class="col-md-3">
-                        <label>Harga</label>
-                        <input type="number" name="items[${itemIndex}][harga]" class="form-control" required>
-                    </div>
-                    <div class="col-md-2">
-                        <label>&nbsp;</label>
-                        <button type="button" class="btn btn-danger remove-item">Hapus</button>
-                    </div>
-                </div>
-            `;
-            $('#items-container').append(newItemRow);
-            itemIndex++; // Increment item index
-
-            $('.remove-item').off('click').on('click', function() {
-                $(this).closest('.item-row').remove();
+        },
+        submitHandler: function(form) {
+            $.ajax({
+                url: form.action,
+                type: form.method,
+                data: $(form).serialize(),
+                success: function(response) {
+                    if (response.status) {
+                        $('#myModal').modal('hide');
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Berhasil',
+                            text: response.message
+                        });
+                        dataPenjualan.ajax.reload(); 
+                    } else {
+                        $('.error-text').text('');
+                        $.each(response.msgField, function(prefix, val) {
+                            $('#error-' + prefix).text(val[0]);
+                        });
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Terjadi Kesalahan',
+                            text: response.message
+                        });
+                    }
+                }
             });
-        });
-
-        $(document).on('click', '.remove-item', function() {
-            $(this).closest('.item-row').remove();
-        });
+            return false; 
+        },
+        errorElement: 'span',
+        errorPlacement: function(error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.form-group').append(error);
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        }
     });
+
+    // Adding new item row
+    $('#add-item').on('click', function() {
+        const newItemRow = `
+            <div class="row item-row">
+                <div class="col-md-4">
+                    <label>Nama Barang</label>
+                    <select name="items[${itemIndex}][barang_id]" class="form-control check-harga" required>
+                        <option value="" data-harga="">- Pilih Barang -</option>
+                        @foreach($barang as $item)
+                            <option value="{{ $item->barang_id }}" data-harga="{{ $item->barang_harga }}">{{ $item->barang_nama }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label>Jumlah</label>
+                    <input type="number" name="items[${itemIndex}][jumlah]" class="form-control" required>
+                </div>
+                <div class="col-md-3">
+                    <label>Harga</label>
+                    <input type="number" name="items[${itemIndex}][harga]" class="form-control harga" required>
+                </div>
+                <div class="col-md-2">
+                    <label>&nbsp;</label>
+                    <button type="button" class="btn btn-danger remove-item">Hapus</button>
+                </div>
+            </div>
+        `;
+        $('#items-container').append(newItemRow);
+        itemIndex++; 
+    });
+
+    $(document).on('click', '.remove-item', function() {
+        $(this).closest('.item-row').remove();
+    });
+});
+
 </script>
