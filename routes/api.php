c565e6bcf88a1d679bb057a2d0a4d06f4fbd7327
nauqi-1 <nauqi2208@gmail.com>
2024-10-28 20:00:02 +0700

    Jobsheet 10 Tugas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APIController;
@@ -32,8 +35,27 @@ Route::group(['prefix'=>'levels'], function(){
     Route::delete('/{level}', [LevelController::class, 'destroy']);
 }); 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=>'users'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{user}', [UserController::class, 'show']);
+    Route::put('/{user}', [UserController::class, 'update']);
+    Route::delete('/{user}', [UserController::class, 'destroy']);
+}); 
+Route::group(['prefix'=>'kategoris'], function(){
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{kategori}', [KategoriController::class, 'show']);
+    Route::put('/{kategori}', [KategoriController::class, 'update']);
+    Route::delete('/{kategori}', [KategoriController::class, 'destroy']);
+});
+
+Route::group(['prefix'=>'barangs'], function(){
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{barang}', [BarangController::class, 'show']);
+    Route::put('/{barang}', [BarangController::class, 'update']);
+    Route::delete('/{barang}', [BarangController::class, 'destroy']);
 });
 
 //route utk API mobile
